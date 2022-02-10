b2d918fe339e4b5ed2725533e99bcc2a205254d4
Viacheslav Paramonov <info@paramonov.by>
2022-02-10 18:22:23 +0800

    Update path values, OS module apply

--- senbazuru/framefinder/const.py
+++ senbazuru/framefinder/const.py
@@ -3,28 +3,29 @@ Created on Sep 26, 2012
 
 @author: cz
 '''
-
+import os
 
 # directory to store the original spreadsheets
-_sheetdir = '../data/testsheets'
+_sheetdir = os.path.abspath('../data/testsheets')
+
 # directory to store the output:
 # each spreadsheet labeled with semantic labels for each row
-_outputdir = '../data/predictsheets'
+_outputdir = os.path.abspath('../data/predictsheets')
 
 # directories to store intermediate results
-_crftempdir = '../data/temp'
-_crffeadir = _crftempdir + '/crf_fea'
-_crfpredictdir = _crftempdir + '/crf_predict'
+_crftempdir = os.path.abspath('../data/temp')
+_crffeadir = os.path.join(_crftempdir, 'crf_fea')
+_crfpredictdir = os.path.join(_crftempdir, 'crf_predict')
 
 # template file for CRF++ to parse the provided features
-_crfpptemplatepath = '../data/template'
+_crfpptemplatepath = os.path.abspath('../data/template')
 # training data provided for 100 spreadsheets downloaded from http://www.census.gov/
-_crftraindatapath = '../data/saus_train.data'
+_crftraindatapath = os.path.abspath('../data/saus_train.data')
 
 ##################################################
 # please specifiy the directory of CRF++
 ##################################################
 # directory of installed CRF++
-_crfppdir = '/home/cz/tools/CRF++-0.58'
+_crfppdir = os.path.abspath('C:\CRF++-0.54')
 
 
